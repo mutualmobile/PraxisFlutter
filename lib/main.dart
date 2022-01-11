57f39ead909b978bde536d8e0d48c4a0a539f02b
Anmol Verma <anmol.verma4@gmail.com>
2022-01-11 18:18:40 +0530

    added: firebase dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:praxis_flutter/application/platform_app.dart';
+import 'package:praxis_flutter/firebase_options.dart';// not committed to git!
 
-void main() {
+void main() async {
   GoRouter.setUrlPathStrategy(UrlPathStrategy.path);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PraxisApp());
 }
-
