bd94c76b7477a88b5e24d87d5d043d580e921ad2
Shubham Singh <shubham.singh@mutualmobile.com>
2022-03-29 12:23:38 +0530

    Migrated from 'http' to 'dio', and from 'logging' to 'logger' libraries

--- praxis_data/lib/network/http_helper.dart
+++ praxis_data/lib/network/http_helper.dart
@@ -1,76 +1,83 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart' as dio;
+import 'package:logger/logger.dart';
 import 'package:praxis_data/network/exceptions/api_exception.dart';
 
 class HttpHelper {
-
   /// [url] can either be a `string` or a `Uri`.
   /// The [type] can be any of the [RequestType]s.
   /// [body] and [encoding] only apply to [RequestType.post] and [RequestType.put] requests. Otherwise, they have no effect.
   /// This is optimized for requests that anticipate a response body of type `Map<String, dynamic>`, as in a json file-type response.
-  static Future<Map<String, dynamic>> invokeHttp(dynamic url, RequestType type, {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
-    http.Response response;
-    Map<String, dynamic> responseBody;
+  static Future<Map<String, dynamic>> invokeHttp(dynamic url, RequestType type,
+      {Map<String, String>? headers, dynamic body}) async {
+    dio.Response response;
     try {
-      response = await _invoke(url, type, headers: headers, body: body, encoding: encoding);
+      response =
+          await _invoke(url.toString(), type, headers: headers, body: body);
     } catch (error) {
       rethrow;
     }
-    responseBody = jsonDecode(response.body);
-    return responseBody;
+    return response.data;
   }
 
   /// [url] can either be a `string` or a `Uri`.
   /// The [type] can be any of the [RequestType]s.
   /// [body] and [encoding] only apply to [RequestType.post] and [RequestType.put] requests. Otherwise, they have no effect.
   /// This is optimized for requests that anticipate a response body of type `List<dynamic>`, as in a list of json objects.
-  static Future<List<dynamic>> invokeHttp2(dynamic url, RequestType type, {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
-    http.Response response;
+  static Future<List<dynamic>> invokeHttp2(dynamic url, RequestType type,
+      {Map<String, String>? headers, dynamic body}) async {
+    dio.Response? response;
     List<dynamic> responseBody;
     try {
-      response = await _invoke(url, type, headers: headers, body: body, encoding: encoding);
+      response =
+          await _invoke(url.toString(), type, headers: headers, body: body);
     } on APIException {
       rethrow;
     } on SocketException {
       rethrow;
+    } catch (exception) {
+      print("Exception is: $exception");
     }
 
-    responseBody = jsonDecode(response.body);
+    responseBody = jsonDecode(response?.data);
     return responseBody;
   }
 
-  static Future<http.Response> _invoke(dynamic url, RequestType type, {Map<String, String>? headers, dynamic body, Encoding? encoding}) async {
-    http.Response response;
+  static Future<dio.Response> _invoke(dynamic url, RequestType type,
+      {Map<String, String>? headers, dynamic body}) async {
+    dio.Response response;
+    dio.Dio client = dio.Dio();
+    final Logger logger = Logger();
     try {
       switch (type) {
         case RequestType.get:
-          response = await http.get(url, headers: headers);
+          response =
+              await client.get(url, options: dio.Options(headers: headers));
           break;
         case RequestType.post:
-          response = await http.post(url,
-              headers: headers, body: body, encoding: encoding);
+          response = await client.post(url,
+              options: dio.Options(headers: headers), data: body);
           break;
         case RequestType.put:
-          response = await http.put(url,
-              headers: headers, body: body, encoding: encoding);
+          response = await client.put(url,
+              options: dio.Options(headers: headers), data: body);
           break;
         case RequestType.delete:
-          response = await http.delete(url, headers: headers);
+          response =
+              await client.delete(url, options: dio.Options(headers: headers));
           break;
       }
 
       if (response.statusCode != 200) {
-        Map<String, dynamic> body = jsonDecode(response.body);
-        throw APIException(body['message'], response.statusCode, body['statusText']);
+        Map<String, dynamic> body = jsonDecode(response.data);
+        throw APIException(
+            body['message'], response.statusCode ?? 0, body['statusText']);
       }
       return response;
-    } on http.ClientException {
-      rethrow;
-    } on SocketException catch(e) {
-      throw Exception(e.osError?.message);
     } catch (error) {
+      logger.e("Caught error!", error);
       rethrow;
     }
   }
