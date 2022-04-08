4f9cbac2938503ebe55328650d593c2644ef3cc2
Anmol Verma <anmol.verma4@gmail.com>
2022-04-08 16:19:44 +0530

    added splash test

---
+++ integration_test/splash/_splash_test_io.dart
@@ -0,0 +1,43 @@
+import 'dart:io' show Platform;
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:praxis_flutter/main.dart' as app;
+import 'package:praxis_flutter/presentation/core/widgets/platform_button.dart';
+
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  testWidgets('verify text', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    app.main();
+
+    // Trace the timeline of the following operation. The timeline result will
+    // be written to `build/integration_response_data.json` with the key
+    // `timeline`.
+    await (binding as IntegrationTestWidgetsFlutterBinding)
+        .traceAction(() async {
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+      final button = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is PraxisButton && widget.title == "Press Me!",
+      );
+      expect(
+        button,
+        findsOneWidget,
+      );
+      (button.evaluate().first.widget as PraxisButton).onPressed();
+      await tester.pump(const Duration(milliseconds: 500));
+
+      expect(
+        find.byWidgetPredicate(
+              (Widget widget) =>
+          widget is PraxisButton && widget.title == "You Pressed me! woo hoo!",
+        ),
+        findsOneWidget,
+      );
+
+    });
+  });
+}
