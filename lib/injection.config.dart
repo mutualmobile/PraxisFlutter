52fd87beec32b654e3a3a13e3d7022dede6115d4
Anmol Verma <anmol.verma4@gmail.com>
2022-04-09 12:44:03 +0530

    injection fixes, add login usecase

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -6,15 +6,22 @@
 
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:praxis_flutter_domain/use_cases/login_use_case.dart' as _i4;
 
+import 'features/login/login_vm.dart' as _i3;
 import 'infrastructure/notifications/firebase_messaging.dart'
-    as _i3; // ignore_for_file: unnecessary_lambdas
+    as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
-/// initializes the registration of provided dependencies inside of [GetIt]
-_i1.GetIt $initGetIt(_i1.GetIt get,
-    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
-  final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.factory<_i3.PraxisFirebaseMessaging>(() => _i3.PraxisFirebaseMessaging());
-  return get;
+/// an extension to register the provided dependencies inside of [GetIt]
+extension GetItInjectableX on _i1.GetIt {
+  /// initializes the registration of provided dependencies inside of [GetIt]
+  _i1.GetIt $initGetIt(
+      {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+    final gh = _i2.GetItHelper(this, environment, environmentFilter);
+    gh.factory<_i3.LoginVM>(() => _i3.LoginVM(get<_i4.LoginUseCase>()));
+    gh.factory<_i5.PraxisFirebaseMessaging>(
+        () => _i5.PraxisFirebaseMessaging());
+    return this;
+  }
 }
