e5c212a29d15a8580bc75eb398e19fff0743eea2
anmolvermamm <anmolvermamm@users.noreply.github.com>
2022-04-13 16:12:59 +0000

    deploy: 3e5648261b55d0598f4c25b1d4bdf4ccdea98a57

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,16 +3,16 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "df294e3433d45ebd3e6f0330ce4d15f7",
-"index.html": "e6a9cb0a692de6f5c900371a94050e60",
-"/": "e6a9cb0a692de6f5c900371a94050e60",
+  "main.dart.js": "282385e49b8aac86aa7016daa9570b26",
+"index.html": "d20eada7f89481833a22fb431d97583b",
+"/": "d20eada7f89481833a22fb431d97583b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "70e0e105cb90e0a5455754e37263da81",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/NOTICES": "466afe18a71b5c9ee7209e479c1a1c17",
+"assets/NOTICES": "5af88641e67980c98b6e1c7ac44cbe72",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
