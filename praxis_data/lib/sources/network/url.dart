cc737b58aea420bd5f169cc9f0c0fcef03d0c72c
Anmol Verma <anmol.verma4@gmail.com>
2022-09-06 12:55:09 +0530

    fix base url

--- praxis_data/lib/sources/network/url.dart
+++ praxis_data/lib/sources/network/url.dart
@@ -1,5 +1,5 @@
 class URL {
-  static const String baseUrl = 'api.icndb.com';
+  static const String baseUrl = 'https://api.chucknorris.io';
   static const String fiveRandomJokesUrl = '$baseUrl/jokes/random/5';
   static const String fiveRandomJokesPath = '/jokes/random/5';
 }
